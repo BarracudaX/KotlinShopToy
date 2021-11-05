7027a245a3c142cdad1db3ef7db64e7ddc62937c
Kalaitsidis Arslan(Dimitris) <a.kalaitsidis@msensis.com>
2021-11-05 14:36:59 +0200

    No longer extendin JpaRepository inteface,but rather Repository with some manually defined methods to reduce the amount of methods of the Dao inteface

--- src/main/java/arslan/test/dao/Dao.kt
+++ src/main/java/arslan/test/dao/Dao.kt
@@ -1,9 +1,23 @@
 package arslan.test.dao
 
-import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.NoRepositoryBean
+import org.springframework.data.repository.Repository
+import java.util.*
 
 @NoRepositoryBean
-interface Dao<T> : JpaRepository<T,Long> {
+interface Dao<T> : Repository<T, Long> {
 
+    fun <S : T> save(entity: S): S
+
+    fun findById(id: Long): Optional<T>
+
+    fun deleteById(id: Long)
+
+    fun delete(entity: T)
+
+    fun findAll(pageable: Pageable): Page<T>
+
+    fun flush()
 }
